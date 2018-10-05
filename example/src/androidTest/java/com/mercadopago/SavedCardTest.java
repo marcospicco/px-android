a4a97d0e1bf463cb752f845d456c59a21018948e
valeserber <valserber@gmail.com>
2018-10-05 17:25:12 -0300

    congrats page button click

--- example/src/androidTest/java/com/mercadopago/SavedCardTest.java
+++ example/src/androidTest/java/com/mercadopago/SavedCardTest.java
@@ -1,20 +1,11 @@
 package com.mercadopago;
 
-import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import com.mercadopago.android.px.configuration.PaymentConfiguration;
-import com.mercadopago.android.px.core.MercadoPagoCheckout;
-import com.mercadopago.android.px.internal.features.plugins.SamplePaymentProcessorNoView;
-import com.mercadopago.android.px.model.GenericPayment;
-import com.mercadopago.android.px.model.Payment;
-import com.mercadopago.android.px.model.PaymentMethods;
-import com.mercadopago.android.px.preferences.CheckoutPreference;
 import com.mercadopago.android.px.testcheckout.flows.SavedCardTestFlow;
 import com.mercadopago.android.px.testcheckout.idleresources.CheckoutResource;
 import com.mercadopago.android.px.testcheckout.pages.CongratsPage;
-import com.mercadopago.android.px.utils.OneTapSamples;
 import com.mercadopago.android.testlib.HttpResource;
 import org.junit.Rule;
 import org.junit.Test;
@@ -25,6 +16,7 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class SavedCardTest {
+
     @Rule
     public HttpResource httpResource = new CheckoutResource();
 
